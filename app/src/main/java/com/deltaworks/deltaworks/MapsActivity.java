317426193dcda63f2ce40b24ab33f64bc6a0d35f
Anando Zaman <anando100@gmail.com>
2019-01-26 12:49:11 -0500

    Initial commit

---
+++ app/src/main/java/com/deltaworks/deltaworks/MapsActivity.java
@@ -0,0 +1,57 @@
+package com.deltaworks.deltaworks;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+
+import org.json.JSONException;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng Param = new LatLng(43.2579, -79.9188);
+        mMap.addMarker(new MarkerOptions().position(Param).title("Param"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Param));
+
+        LatLng Seeam = new LatLng(43.26748120000001, -79.91701469999998);
+        mMap.addMarker(new MarkerOptions().position(Seeam).title("Seeam"));
+        
+
+
+    }
+}
